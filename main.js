2c383652d4ca70ad1e91eab4987e1813c3ba649c
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-05-30 09:06:40 -0400

    good starts

---
+++ main.js
@@ -0,0 +1,7 @@
+// https://dev.to/javascriptacademy/form-validation-using-javascript-34je
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
